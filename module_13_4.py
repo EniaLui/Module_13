423beb3cc749f2c3577a984cf8fec5c0cedd5e7d
EniaLui <eniana@mail.ru>
2024-12-22 20:05:47 +0300

    module_13_4.py

--- module_13_4.py
+++ module_13_4.py
@@ -1,5 +1,6 @@
-from aiogram import Bot, Dispatcher, executor, types
+import asyncio
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram import Bot, Dispatcher, executor, types
 from aiogram.dispatcher.filters.state import State, StatesGroup
 
 api = "тут ключ от бота"
